f9c63956b966f44815b95ea2fc97d88743a35637
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-11 12:32:23 +0700

    update

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,9 @@
 const routes = {
-    home: '/',
-    bill: '/bill',
-    product: '/product',
-    login: '/login',
-    register: '/register',
+    home: 'https://Minh-Ngoc.github.io/',
+    bill: 'https://Minh-Ngoc.github.io/bill',
+    product: 'https://Minh-Ngoc.github.io/product',
+    login: 'https://Minh-Ngoc.github.io/login',
+    register: 'https://Minh-Ngoc.github.io/register',
 }
 
 export default routes;
